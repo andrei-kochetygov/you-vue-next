dc0785e6b1b229e1a644251e819c34899be7c564
Kochetygov Andrew <flatdragon@yandex.ru>
2020-06-29 16:49:36 +0300

    Fix issue with empty Q param in URL

--- src/lib/util.js
+++ src/lib/util.js
@@ -2,7 +2,9 @@ export function createUrl (base, query) {
   const url = new URL(base)
 
   for (const key in query) {
-    url.searchParams.set(key, query[key])
+    if (query[key] !== 'undefined' && query[key]) {
+      url.searchParams.set(key, query[key])
+    }
   }
 
   return url
